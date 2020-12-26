091452d093837e46fcd7118f81da4f979bb40d8e
Pooja Nangude <poojanangude@Poojas-MacBook-Air.local>
2020-12-26 14:04:06 +0530

    user regsitration using database

--- user.py
+++ user.py
@@ -1,4 +1,6 @@
 import sqlite3
+from flask_restful import Resource, reqparse
+
 
 class User:
     def __init__(self, _id, username, password):
@@ -27,7 +29,7 @@ class User:
         cursor = connection.cursor()
 
         query = "SELECT * from users WHERE id=?"
-        result = cursor.execute(query,(_id,))
+        result = cursor.execute(query, (_id,))
         row = result.fetchone()
         if row:
             user = cls(*row)
@@ -35,3 +37,32 @@ class User:
             user = None
         connection.close()
         return user
+
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help="This field cannot be blank")
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help="This field cannot be blank")
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        if User.find_by_username(data['username']):
+            return {"message": "A user with that username already exists"},400
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"
+        cursor.execute(query, (data['username'], data['password'],))
+
+        connection.commit()
+        connection.close()
+
+        return {"message": "User created successfully"}
